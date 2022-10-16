4afca7203125d946ce5ca80fdc353ef83891410a
devmoaid1 <moaidmohamed1312@gmail.com>
2022-10-17 00:25:48 +0200

    updated screenshotes readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_task_app/app.dart';
 import 'package:flutter_task_app/core/dependencies.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   setupLocator();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(DevicePreview(builder: ((context) => const App())));
 }
